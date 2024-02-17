c0472f39945a9a4f39adfe2dd36ebc0a8928ef9d
atifpv11 <muhammedatif44@gmail.com>
2024-02-17 19:54:02 +0530

    errors corrected

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -17,18 +17,46 @@ public class Car extends Vehicle {
         this.doors=doors;
         this.gears=gears;
         this.isManual=isManual;
-        this.currentGear=1;
         this.seats=seats;
+
+        this.currentGear=1;
     }
 
     public void changeGear(int newGear){
-        this.currentGear=newGear;
+        currentGear=newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        super.move(newSpeed,newDirection);
+        move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 
+    public int getWheels(){
+        return wheels;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public int getDoors(){
+        return doors;
+    }
+
+    public int getGears(){
+        return gears;
+    }
+
+    public boolean isManual(){
+        return isManual;
+    }
+
+    public int getCurrentGear(){
+        return currentGear;
+    }
+
+    public int getSeats(){
+        return seats;
+    }
 }
